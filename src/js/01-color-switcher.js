0d2c7d37dcfcfca36706c5936f9c4ed2787748e4
AnnaShkandrii <anna.didoshak@aiesec.net>
2023-05-25 19:20:18 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
-
 const refs = {
   start: document.querySelector('button[data-start]'),
   stop: document.querySelector('button[data-stop]'),
 }
 
 let changeBodyColor;
-
 refs.stop.setAttribute("disabled", "disabled");
 refs.start.addEventListener('click', startChangeColor);
 refs.stop.addEventListener('click', stopChangeColor);
